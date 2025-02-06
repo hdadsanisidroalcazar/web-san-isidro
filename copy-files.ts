860c10cd2477cd1523d67252d1d790495a4ac5b3
Eduardo Parra <eduparra90@gmail.com>
2025-02-06 22:59:06 +0100

    Fix prettier

--- copy-files.ts
+++ copy-files.ts
@@ -1,24 +1,24 @@
-import type { AstroIntegration } from "astro";
-import { promises as fs } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import type { AstroIntegration } from 'astro';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Utility function for formatted logging
 function formatLog(tag: string, message: string) {
-  const timestamp = new Date().toLocaleTimeString("en-US", {
-    hour: "2-digit",
+  const timestamp = new Date().toLocaleTimeString('en-US', {
+    hour: '2-digit',
     hour12: false,
-    minute: "2-digit",
-    second: "2-digit",
+    minute: '2-digit',
+    second: '2-digit',
   });
 
   // eslint-disable-next-line no-console
   console.log(
-    "\n" + // Add space above
+    '\n' + // Add space above
       `\x1b[90m${timestamp}\x1b[0m ` + // Gray timestamp
       `[\x1b[36m${tag}\x1b[0m] ` + // Cyan colored tag
       `${message}` + // Message
-      "\n", // Add space below
+      '\n' // Add space below
   );
 }
 
@@ -43,16 +43,16 @@ async function copyFiles(srcDir: string, destDir: string) {
 export function CopyFilesPlugin(): AstroIntegration {
   return {
     hooks: {
-      "astro:build:done": async ({ dir }) => {
-        formatLog("copy-files", "Copying files to .vercel/output/static");
+      'astro:build:done': async ({ dir }) => {
+        formatLog('copy-files', 'Copying files to .vercel/output/static');
 
         const distDir = fileURLToPath(dir.href);
-        const staticDir = path.resolve(".vercel/output/static");
+        const staticDir = path.resolve('.vercel/output/static');
 
         await fs.mkdir(staticDir, { recursive: true });
         await copyFiles(distDir, staticDir);
       },
     },
-    name: "copy-files",
+    name: 'copy-files',
   };
 }
